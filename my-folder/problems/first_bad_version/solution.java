c860666c9762b6adeba7fcc2e7646a6761d88901
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2022-07-30 06:48:43 +0000

    Sync LeetCode submission - First Bad Version (java)

---
+++ my-folder/problems/first_bad_version/solution.java
@@ -0,0 +1,22 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l = 1, h = n, mid=0,ans=0;
+        
+        while(l <= h) {
+            mid = h + (l - h)/2;
+            if(isBadVersion(mid)==true) {
+                // if(isBadVersion(mid-1) == false)
+                //     return mid;
+                // else
+                ans=mid;
+                    h = mid - 1;
+            }
+            else
+                l = mid + 1;
+        }
+        return ans;
+    }
+}
