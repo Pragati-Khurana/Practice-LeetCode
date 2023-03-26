d2628839c977eeba677161ee1723c679ffb746b5
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-03-26 11:23:42 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (java)

--- my-folder/problems/find_first_and_last_position_of_element_in_sorted_array/solution.java
+++ my-folder/problems/find_first_and_last_position_of_element_in_sorted_array/solution.java
@@ -1,34 +1,32 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int l=0, h=nums.length-1, mid=0;
-        int[] res = new int[2];
-        res[0] = -1;
-        res[1] = -1;
-        int flag = 0;
-        
-        while(l <= h) {
-            mid = h + (l-h)/2;
-            if(nums[mid] == target) {
-                res[0] = mid;
-                h = mid - 1;
-            }
-            else if(target > nums[mid])
-                l = mid + 1;
-            else if(target < nums[mid])
-                h = mid - 1;
-        }
-        
-        l=0; h=nums.length-1; mid=0;
+        int l=0;
+        int h=nums.length-1;
+        int res[] = {-1,-1};
+
         while(l<=h) {
-            mid = h + (l-h)/2;
-            if(nums[mid] == target) {
-                res[1] = mid;
-                l = mid + 1;
+            int mid=(l+h)/2;
+            if(target==nums[mid]) {
+                res[0]=mid;
+                h=mid-1;
+            } else if(target > nums[mid]) {
+                l=mid+1;
+            } else if(target < nums[mid]) {
+                h=mid-1; 
             }
-            else if(target > nums[mid])
-                l = mid + 1;
-            else if(target < nums[mid])
-                h = mid - 1;
+        }
+        l=0;
+        h=nums.length-1;
+        while(l<=h) {
+            int mid=(l+h)/2;
+            if(target==nums[mid]) {
+                res[1]=mid;
+                l=mid+1;
+            } else if(target > nums[mid]) {
+                l=mid+1;
+            } else if(target < nums[mid]) {
+                h=mid-1; 
+            }
         }
         return res;
     }
