20cbfa62cd2c24f66d4f316db966785271ee3222
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-03-30 07:10:40 +0000

    Sync LeetCode submission - Single Element in a Sorted Array (java)

--- my-folder/problems/single_element_in_a_sorted_array/solution.java
+++ my-folder/problems/single_element_in_a_sorted_array/solution.java
@@ -1,9 +1,21 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int xor=0;
-        for(int i=0;i<nums.length;i++) {
-            xor = xor ^ nums[i];
+        int l=0, h=nums.length-2, mid;
+
+        while(l<=h) {
+            mid=l+(h-l)/2;
+            if(mid%2==0) {
+                if(nums[mid]==nums[mid+1])
+                    l=mid+1;
+                else
+                    h=mid-1;
+            } else {
+                if(nums[mid]==nums[mid+1])
+                    h=mid-1;
+                else
+                    l=mid+1;
+            }
         }
-        return xor;
+        return nums[l];
     }
 }
