d45dd270bc98246a76acb6e2c3f6cf00eaa04ca5
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-03-31 05:39:51 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- my-folder/problems/valid_anagram/solution.java
+++ my-folder/problems/valid_anagram/solution.java
@@ -1,14 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] ch1 = s.toCharArray();   
-        char[] ch2 = t.toCharArray();   
+        HashMap<Character, Integer> m1 = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> m2 = new HashMap<Character, Integer>();
 
-        Arrays.sort(ch1);
-        Arrays.sort(ch2);
-        
-        if(String.valueOf(ch1).equals(String.valueOf(ch2)))
+        char ch1[] = s.toCharArray();
+        char ch2[] = t.toCharArray();
+
+        for(int i=0;i<ch1.length;i++) {
+            m1.put(ch1[i], m1.getOrDefault(ch1[i],0)+1);
+        }
+
+        for(int i=0;i<ch2.length;i++) {
+            m2.put(ch2[i], m2.getOrDefault(ch2[i],0)+1);
+        }
+
+        if(m1.equals(m2))
             return true;
-
         return false;
     }
 }
