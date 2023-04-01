5316aa19343d52f5a218725a2e750eb0b52453b1
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-04-01 09:46:04 +0000

    Sync LeetCode submission - Roman to Integer (java)

---
+++ my-folder/problems/roman_to_integer/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int romanToInt(String s) {
+        char ch[] = s.toCharArray();
+        HashMap<Character, Integer> m = new HashMap<Character, Integer>();
+        
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+
+        int sum=0, i;
+        for(i=0;i<ch.length-1;i++) {
+            if(m.get(ch[i])<m.get(ch[i+1]))
+                sum=sum+(-m.get(ch[i]));
+            else
+                sum=sum+m.get(ch[i]);
+        }
+        sum=sum+m.get(ch[i]);
+
+        return sum;
+    }
+}
