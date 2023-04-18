dfe35c9c9d80711e283d0e365bed86aeefbe347f
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-04-18 19:46:17 +0000

    Sync LeetCode submission - Power of Two (java)

--- my-folder/problems/power_of_two/solution.java
+++ my-folder/problems/power_of_two/solution.java
@@ -1,12 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        while(n%2==0 && n>1)
-        {
-            n=n/2;
-        }
-        if(n==1)
+        if(n>0 && (n&(n-1))==0)
             return true;
-        else
-            return false;
-}
+        return false;
+    }
 }
