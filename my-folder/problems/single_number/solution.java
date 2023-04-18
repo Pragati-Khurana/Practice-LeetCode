9fa56c7c43f01dc5e8d9b89d4193c668e56fe1f6
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-04-18 20:02:03 +0000

    Sync LeetCode submission - Single Number (java)

--- my-folder/problems/single_number/solution.java
+++ my-folder/problems/single_number/solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i=i+2) {
-            if(nums[i]!=nums[i+1])
-                return nums[i];
+        int res=0;
+        for(int i=0;i<nums.length;i++) {
+            res = res^nums[i];
         }
-        return nums[nums.length-1];
+        return res;
     }
 }
