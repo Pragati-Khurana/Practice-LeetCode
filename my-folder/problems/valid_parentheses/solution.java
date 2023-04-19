be562b2accf8e79979de4972125754bda85b274b
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-04-19 20:04:14 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- my-folder/problems/valid_parentheses/solution.java
+++ my-folder/problems/valid_parentheses/solution.java
@@ -1,36 +1,32 @@
 class Solution {
-    public boolean isValid(String str) {
-        char ch[] = str.toCharArray();
-        
-        Stack<Character> s = new Stack<>();
+    public boolean isValid(String s) {
+        Stack<Character> s1 = new Stack<>();
+        char ch[] = s.toCharArray();
         boolean res=false;
-        
+
         for(int i=0;i<ch.length;i++) {
-            if(ch[i]=='(' || ch[i]=='{' || ch[i]=='[') {
-                s.push(ch[i]);
-            }
+            if(ch[i]=='(' || ch[i]=='[' || ch[i]=='{')
+                s1.push(ch[i]);
             else {
-                
-                char p;
-                if(!s.isEmpty())
-                    p = s.pop();
-                else
-                    return false;
-                
-                if(p=='(' && ch[i]==')')
-                    res=true;
-                else if(p=='{' && ch[i]=='}')
-                    res=true;
-                else if(p=='[' && ch[i]==']')
-                    res=true;
-                else {
+                if(!s1.empty()) {
+                    char pop = s1.pop();
+                    if(ch[i]==')' && pop=='(')
+                        res = true;
+                    else if(ch[i]==']' && pop=='[')
+                        res = true;
+                    else if(ch[i]=='}' && pop=='{')
+                        res = true;
+                    else {
+                        res = false;
+                        break;
+                    }
+                }  else {
                     res = false;
                     break;
                 }
             }
         }
-        
-        if(!s.isEmpty())
+        if(!s1.empty())
             res = false;
         return res;
     }
