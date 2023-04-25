1d839cd764184d9df3774f8105e0cea3c954605a
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-04-25 07:00:07 +0000

    Sync LeetCode submission - Next Greater Element I (java)

--- my-folder/problems/next_greater_element_i/solution.java
+++ my-folder/problems/next_greater_element_i/solution.java
@@ -1,26 +1,23 @@
 class Solution {
-    public void fillStack(int[] nums2, Stack<Integer> s1) {
-        for(int i=0;i<nums2.length;i++) {
-            s1.push(nums2[i]);
-        }
-    }
-
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> s1 = new Stack<>();
-        int[] ans = new int[nums1.length];
-         Arrays.fill(ans, -1);
-        for(int i=0;i<nums1.length;i++) {
-            s1.clear();
-            fillStack(nums2, s1);
-            int temp = s1.peek();
-            while(temp!=nums1[i]) {
-                if(temp>nums1[i]) {
-                    ans[i] = temp;
-                }
-                s1.pop();
-                temp=s1.peek();
-            }
+        HashMap<Integer, Integer> m = new HashMap<>();
+        Stack<Integer> s = new Stack<>();
+        int[] res = new int[nums1.length];
+
+        for(int i=nums2.length-1;i>=0;i--) {
+            while(s.empty()==false && s.peek()<nums2[i])
+                s.pop();
+            if(s.empty()==false)
+                m.put(nums2[i], s.peek());
+            else
+                m.put(nums2[i], -1);
+
+            s.push(nums2[i]);
         }
-        return ans;
+
+        for(int i=0;i<nums1.length;i++) {
+            res[i] = m.get(nums1[i]);
+        }
+        return res;
     }
 }
