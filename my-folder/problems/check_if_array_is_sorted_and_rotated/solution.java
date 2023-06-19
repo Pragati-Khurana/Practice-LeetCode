bf3d6f22100e7767235127fad4159d33f31eb5b8
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-06-19 19:37:48 +0000

    Sync LeetCode submission - Check if Array Is Sorted and Rotated (java)

--- my-folder/problems/check_if_array_is_sorted_and_rotated/solution.java
+++ my-folder/problems/check_if_array_is_sorted_and_rotated/solution.java
@@ -1,30 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
-    public void reverse (int[] nums, int l, int r) {
-        while(l < r) {
-            int temp = nums[l];
-            nums[l] = nums[r];
-            nums[r] = temp;
-            l++;
-            r--;
-        }
-    }
     public boolean check(int[] nums) {
-        int[] sortedArr = nums.clone();
-        Arrays.sort(sortedArr);
-        int[] oriArr = nums.clone();
-
-        for(int i=0;i<nums.length;i++) {
-            int k = i + 1;
-            nums = oriArr.clone();
-            reverse(nums, 0, nums.length-k-1);
-            reverse(nums, nums.length-k, nums.length-1);
-            reverse(nums, 0, nums.length-1);
-
-            if(Arrays.equals(nums,sortedArr))
-                return true;
+        int flag=0;
+        for(int i=0;i<nums.length-1;i++) {
+            if(nums[i]>nums[i+1]) {
+                if(flag == 0) {
+                    int k=i+1;
+                    while(k<nums.length) {
+                        if(nums[k]>nums[0])
+                            return false;
+                        k++;
+                    }
+                    flag=1;
+                } else {
+                    return false;
+                }
+            }
         }
-        return false;
+        return true;
     }
 }
