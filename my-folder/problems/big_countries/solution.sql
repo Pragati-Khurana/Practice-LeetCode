e4577957c9c9653b0c443dc0757bc2d7e59badd2
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-09-12 06:15:03 +0000

    Sync LeetCode submission - Big Countries (mssql)

---
+++ my-folder/problems/big_countries/solution.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+-- select name, population, area from World
+-- where area>=3000000 or population>=25000000;
+
+select area,population,name
+from world
+where area>=3000000
+union
+select area,population,name
+from world
+where population>=25000000
