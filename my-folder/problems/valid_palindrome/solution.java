4cdc1638f88cc7fe689cd9e3b11bd3a9ea415d35
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2023-12-17 05:23:19 +0000

    Sync LeetCode submission - Valid Palindrome (java)

--- my-folder/problems/valid_palindrome/solution.java
+++ my-folder/problems/valid_palindrome/solution.java
@@ -1,24 +1,15 @@
 class Solution {
-    public void reverse(char[] ch, int n, int i) {
-        if(i>=n/2)
-            return;
-        char temp = ch[i];
-        ch[i] = ch[n-i-1];
-        ch[n-i-1] = temp;
-        reverse(ch, n, i+1);
-    }
-
-    public boolean isPalindrome(String s) {
-        s = s.replaceAll("\\s", "");
-        s = s.replaceAll("[^a-zA-Z0-9]","");
-        s = s.toLowerCase();
-        char ch[] = s.toCharArray();
-        reverse(ch, s.length(), 0);
-        String rs = String.valueOf(ch);
-
-        if(s.equals(rs))
+    public static boolean checkPalindrome(int i, String s) {
+        if(i >= s.length()-i-1)
             return true;
-        else
+
+        if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(s.length()-i-1)))
             return false;
+
+        return checkPalindrome(i+1, s);
+    }
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        return checkPalindrome(0, s);
     }
 }
