5629273f7c70eb88ec3dd614dc62afbc5b0525fa
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2024-06-17 18:37:41 +0000

    Sync LeetCode submission - Customer Who Visited but Did Not Make Any Transactions (mssql)

--- my-folder/problems/customer_who_visited_but_did_not_make_any_transactions/solution.sql
+++ my-folder/problems/customer_who_visited_but_did_not_make_any_transactions/solution.sql
@@ -1,13 +1,4 @@
-# Write your MySQL query statement below
-# select v.customer_id, count() from Visits v
-
-select customer_id, count(customer_id) as count_no_trans
-from Visits
-where visit_id not in
-(select visit_id from Transactions)
-group by customer_id
-order by count(customer_id) desc
-
-# select v.visit_id from Visits v
-# inner join Transactions t
-# on v.visit_id<>t.visit_id
+/* Write your T-SQL query statement below */
+SELECT CUSTOMER_ID, COUNT(CUSTOMER_ID) AS COUNT_NO_TRANS FROM VISITS V
+WHERE VISIT_ID NOT IN (SELECT DISTINCT VISIT_ID FROM TRANSACTIONS)
+GROUP BY CUSTOMER_ID;
