b7bb91e93fa6189379ae108717da065565721dfd
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2024-06-17 18:29:08 +0000

    Sync LeetCode submission - Product Sales Analysis I (mssql)

--- my-folder/problems/product_sales_analysis_i/solution.sql
+++ my-folder/problems/product_sales_analysis_i/solution.sql
@@ -1,6 +1,4 @@
-# Write your MySQL query statement below
-select p.product_name, s.year, s.price
-from Sales s
-left join
-Product p
-on p.product_id=s.product_id;
+/* Write your T-SQL query statement below */
+SELECT P.PRODUCT_NAME, S.YEAR, S.PRICE FROM PRODUCT P
+INNER JOIN SALES S
+    ON P.PRODUCT_ID = S.PRODUCT_ID
