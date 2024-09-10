cbf87be9d3fc7b4154e1b32d69073ea036a731d9
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2024-09-10 19:04:32 +0000

    Sync LeetCode submission - Fibonacci Number (java)

--- my-folder/problems/fibonacci_number/solution.java
+++ my-folder/problems/fibonacci_number/solution.java
@@ -1,19 +1,8 @@
 class Solution {
     public int fib(int n) {
-        int f0 = 0;
-        int f1 = 1;
-        int f2 = f0 + f1;
+        if(n <= 1)
+            return n;
 
-        if(n==0) return f0;
-        if(n==1) return f1;
-        
-        int i=2;
-        while(i<n) {
-            f0 = f1;
-            f1 = f2;
-            f2 = f0 + f1;
-            i++;
-        }
-        return f2;
+        return fib(n-1) + fib(n-2);
     }
 }
