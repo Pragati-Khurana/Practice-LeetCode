25cb00824e6e6fbbdcccfdd727451684511f7aff
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2024-09-18 19:17:42 +0000

    Sync LeetCode submission - Missing Number (java)

--- my-folder/problems/missing_number/solution.java
+++ my-folder/problems/missing_number/solution.java
@@ -1,11 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int sum = (n*(n+1))/2;
+        int xor1=0, xor2=0;
+        for(int i=0; i<n; i++) {
+            xor1 = xor1 ^ (i+1);
+            xor2 = xor2 ^ nums[i];
+        }
 
-        for(int i=0;i<n;i++)
-            sum = sum - nums[i];
-
-        return sum;   
+        return (xor1 ^ xor2);
     }
 }
