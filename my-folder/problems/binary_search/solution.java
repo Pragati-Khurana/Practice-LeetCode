26113afb23f5bc1099c521298eb7c79fead8cfea
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2024-09-22 13:03:46 +0000

    Sync LeetCode submission - Binary Search (java)

--- my-folder/problems/binary_search/solution.java
+++ my-folder/problems/binary_search/solution.java
@@ -1,15 +1,17 @@
 class Solution {
+    public int BST(int[] nums, int l, int h, int target) {
+        int m = (l + h) / 2;
+        if(l>h)
+            return -1;
+        else if(nums[m] == target)
+            return m;
+        else if(nums[m] > target)
+            return BST(nums, l, m-1, target);
+        else
+            return BST(nums, m+1, h, target);
+    }
+
     public int search(int[] nums, int target) {
-        int l=0, h=nums.length-1, mid;
-        while(l<=h) {
-           mid=(l+h)/2;
-           if(target > nums[mid])
-                l = mid + 1;
-            else if(target < nums[mid])
-                h = mid -1;
-            else
-                return mid;
-        }
-        return -1;
+        return BST(nums, 0, nums.length-1, target);
     }
 }
