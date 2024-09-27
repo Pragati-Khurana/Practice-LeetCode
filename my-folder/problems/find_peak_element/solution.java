83874080a643210e8f405c2be25f022b3c107c18
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2024-09-27 18:18:38 +0000

    Sync LeetCode submission - Find Peak Element (java)

--- my-folder/problems/find_peak_element/solution.java
+++ my-folder/problems/find_peak_element/solution.java
@@ -1,15 +1,26 @@
 class Solution {
-    public int findPeak(int[] nums, int l, int h) {
-        int mid = l+(h-l)/2;
-        if((mid==0 || nums[mid]>nums[mid-1]) && (mid==nums.length-1 || nums[mid]>nums[mid+1]))
-            return mid;
-        else if(mid>0 && nums[mid-1]>nums[mid])
-            return findPeak(nums, l, mid-1);
-        else
-            return findPeak(nums, mid+1, h);
-    }
-
     public int findPeakElement(int[] nums) {
-        return findPeak(nums, 0, nums.length-1);
+        if(nums.length == 1)
+            return 0;
+
+        if(nums[0] > nums[1])
+            return 0;
+
+        if(nums[nums.length-1] > nums[nums.length-2])
+            return nums.length-1;
+
+        int l=1, h=nums.length-2, m;
+
+        while(l <= h) {
+            m = (l+h)/2;
+
+            if(nums[m] > nums[m-1] && nums[m] > nums[m+1])
+                return m;
+            else if(nums[m] > nums[m-1])
+                l = m+1;
+            else
+                h = m-1;
+        }
+        return -1;
     }
 }
