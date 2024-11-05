85ac0a820ce103ceda99b98dc6fbaec4f9b2ca44
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2024-11-05 06:22:36 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (java)

--- my-folder/problems/intersection_of_two_linked_lists/solution.java
+++ my-folder/problems/intersection_of_two_linked_lists/solution.java
@@ -10,16 +10,42 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA==null || headB==null) return null;
-
-        ListNode a = headA;
-        ListNode b = headB;
-
-        while(a!=b) {
-            a = a==null ? headB:a.next;
-            b = b==null ? headA:b.next;
+    public int getDifference(ListNode head1, ListNode head2) {
+        int l1=0, l2=0;
+        while(head1 != null || head2 != null) {
+            if(head1 != null) {
+                l1++;
+                head1 = head1.next;
+            }
+            if(head2 != null) {
+                l2++;
+                head2 = head2.next;
+            }
         }
-        return a;
+        return l1-l2;
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int d = getDifference(headA, headB);
+
+        if(d < 0) {
+            while(d < 0) {
+                headB = headB.next;
+                d++;
+            }
+        } else if(d > 0) {
+            while(d > 0) {
+                headA = headA.next;
+                d--;
+            }
+        }
+
+        while(headA != null) {
+            if(headA == headB)
+                return headA;
+
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
     }
 }
