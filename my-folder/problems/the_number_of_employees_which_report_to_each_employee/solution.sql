0bed2a1f8b73697f0671a0b183b271448da4b71f
Pragati Khurana <60056772+Pragati-Khurana@users.noreply.github.com>
2025-06-26 09:19:17 +0000

    Sync LeetCode submission - The Number of Employees Which Report to Each Employee (mssql)

---
+++ my-folder/problems/the_number_of_employees_which_report_to_each_employee/solution.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select m.employee_id, m.name, count(e.employee_id) as reports_count,
+round(avg(e.age * 1.0), 0) as average_age
+from Employees e
+inner join Employees m
+    on e.reports_to = m.employee_id
+group by m.employee_id, m.name
+order by m.employee_id;
